db60e3e12870e754c96602dcf66b6ddd98db14dd
shihab01118 <shahed.shihab.0@gmail.com>
2023-11-06 00:06:37 +0600

    post api added for assignment creation

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jcpqyde.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -17,26 +16,31 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-  
-    
+    const database = client.db("groupStudyDB");
+    const assignmentCollection = database.collection("assignments");
 
+    app.post("/api/v1/user/assignments", async (req, res) => {
+        const assignment = req.body;
+        const result = await assignmentCollection.insertOne(assignment);
+        res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("valo kore study koro");
